e17f37413158a7ab298c8e7e320dc0e9a0e6674a
Boris Nacev <bnachev77@gmail.com>
2024-06-05 03:17:02 +0200

    made banner

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script src="https://kit.fontawesome.com/0d6f25b6d2.js" crossorigin="anonymous"></script>
+</head>
+
+<body>
+    <div class="banner min-h-[60vh] flex flex-col gap-48 bg-[url('./imgs/banner1.jpg')] bg-cover bg-center border-b-2 border-gray-400 ">
+        <nav class="flex justify-between px-10 py-5 justify-self-start">
+            <p class="text-xl text-neutral-300">Brainster Library</p>
+            <div class="buttons flex gap-7">
+                <button class="text-white bg-slate-500 hover:bg-slate-400 focus:outline-none font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2">SignUp</button>
+                <button class="text-white bg-slate-500 hover:bg-slate-400 focus:outline-none font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2">LogIn</button>
+            </div>
+        </nav>
+        <h1 class="text-6xl justify-self-center text-white bg-slate-700 rounded w-1/2 self-end p-6">Welcome to Brainster library</h1>
+    </div>
+
+</body>
+
+</html>
