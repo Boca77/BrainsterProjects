e72306425f960f09f36aa0a8d06c64fdd48667d3
Boris Nacev <bnachev77@gmail.com>
2024-06-05 21:12:22 +0200

    theme chenge

--- index.php
+++ index.php
@@ -14,21 +14,21 @@
         <nav class="flex justify-between px-10 py-5 justify-self-start">
             <p class="text-xl text-neutral-300">Brainster Library</p>
             <div class="buttons flex gap-7">
-                <button class="text-white bg-slate-500 hover:bg-slate-400 focus:outline-none font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2">SignUp</button>
-                <button class="text-white bg-slate-500 hover:bg-slate-400 focus:outline-none font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2">LogIn</button>
+                <button class="text-white bg-[#1B3534] hover:bg-[#2c5755] focus:outline-none font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2">SignUp</button>
+                <button class="text-white bg-[#1B3534] hover:bg-[#2c5755] focus:outline-none font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2">LogIn</button>
             </div>
         </nav>
-        <h1 class="text-6xl justify-self-center text-white bg-slate-700 rounded w-1/2 self-end p-6">Welcome to Brainster library</h1>
+        <h1 class="text-6xl justify-self-center text-white bg-[#1B3534] rounded w-1/2 self-end p-6">Welcome to Brainster library</h1>
     </div>
-    <main class=" bg-slate-600 flex flex-col items-center">
-        <div class="relative w-full flex flex-col justify-center">
-            <div id="filters" class=" px-10 py-5 w-4/5 mx-auto flex border-b-4 border-gray-400">
+    <main class=" bg-[#2A4742] flex flex-col items-center">
+        <div class="relative w-full flex flex-col justify-center ">
+            <div id="filters" class=" px-10 py-5 w-4/5 mx-auto flex border-b-4 border-gray-400 bg-white/20 rounded-b ">
                 <div class="flex items-center gap-2">
                     <i id="filter" class="fa-solid fa-bars fa-xl cursor-pointer"></i>
                     <p>Filters</p>
                 </div>
             </div>
-            <div id="filter-content" class="absolute opacity-0 top-0 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+            <div id="filter-content" class="absolute opacity-0 top-0 left-1/2 transform -translate-x-1/2 -translate-y-1/2 ">
                 <div class="flex items-center gap-5 p-5">
                     <div class="items-center flex">
                         <input checked id="adventure" type="checkbox" value="Adventure" class="w-4 h-4 accent-slate-400">
@@ -64,9 +64,9 @@
         </div>
         <div id="outer" class="flex justify-center w-4/5">
             <div id="book-cards" class="flex w-full md:w-4/5 flex-wrap gap-6 justify-center px-4 my-14">
-                <div id="card" class="w-full shadow-2xl sm:w-1/2 md:w-1/3 lg:w-1/4 ">
+                <div id="card" class="w-full shadow-2xl sm:w-1/2 md:w-1/3 lg:w-1/4 relative ">
                     <img class="rounded-t-lg w-full h-[300px] md:h-[400px] object-cover" src="https://picsum.photos/200/300" alt="">
-                    <div id="content" class="p-2 flex flex-col bg-slate-400 rounded-b-lg">
+                    <div id="content" class="p-2 flex flex-col bg-[#507e76] text-white rounded-b-lg ">
                         <h3 class="text-lg shadow-lg border-b mb-3 self-center">Book Title</h3>
                         <p>Author: Jon</p>
                         <p>Category: <span class="category">Adventure</span></p>
@@ -74,7 +74,7 @@
                 </div>
                 <div id="card" class="w-full shadow-2xl sm:w-1/2 md:w-1/3 lg:w-1/4 ">
                     <img class="rounded-t-lg w-full h-[300px] md:h-[400px] object-cover" src="https://picsum.photos/200/300" alt="">
-                    <div id="content" class="p-2 flex flex-col rounded-b-lg bg-slate-400">
+                    <div id="content" class="p-2 flex flex-col rounded-b-lg bg-[#507e76] text-white">
                         <h3 class="text-lg shadow-lg border-b mb-3 self-center">Book Title</h3>
                         <p>Author: Jon</p>
                         <p>Category: <span class="category">Mystery</span></p>
@@ -82,7 +82,7 @@
                 </div>
                 <div id="card" class="w-full shadow-2xl sm:w-1/2 md:w-1/3 lg:w-1/4 ">
                     <img class="rounded-t-lg w-full h-[300px] md:h-[400px] object-cover" src="https://picsum.photos/200/300" alt="">
-                    <div id="content" class="p-2 flex flex-col rounded-b-lg bg-slate-400">
+                    <div id="content" class="p-2 flex flex-col rounded-b-lg bg-[#507e76] text-white">
                         <h3 class="text-lg shadow-lg border-b mb-3 self-center">Book Title</h3>
                         <p>Author: Jon</p>
                         <p>Category: <span class="category">Adventure</span></p>
@@ -90,7 +90,7 @@
                 </div>
                 <div id="card" class="w-full shadow-2xl sm:w-1/2 md:w-1/3 lg:w-1/4 ">
                     <img class="rounded-t-lg w-full h-[300px] md:h-[400px] object-cover" src="https://picsum.photos/200/300" alt="">
-                    <div id="content" class="p-2 flex flex-col rounded-b-lg bg-slate-400">
+                    <div id="content" class="p-2 flex flex-col rounded-b-lg bg-[#507e76] text-white">
                         <h3 class="text-lg shadow-lg border-b mb-3 self-center">Book Title</h3>
                         <p>Author: Jon</p>
                         <p>Category: <span class="category">Horror</span></p>
@@ -98,7 +98,7 @@
                 </div>
                 <div id="card" class="w-full shadow-2xl sm:w-1/2 md:w-1/3 lg:w-1/4 ">
                     <img class="rounded-t-lg w-full h-[300px] md:h-[400px] object-cover" src="https://picsum.photos/200/300" alt="">
-                    <div id="content" class="p-2 flex flex-col rounded-b-lg bg-slate-400">
+                    <div id="content" class="p-2 flex flex-col rounded-b-lg bg-[#507e76] text-white">
                         <h3 class="text-lg shadow-lg border-b mb-3 self-center">Book Title</h3>
                         <p>Author: Jon</p>
                         <p>Category: <span class="category">Mystery</span></p>
@@ -106,7 +106,7 @@
                 </div>
                 <div id="card" class="w-full shadow-2xl sm:w-1/2 md:w-1/3 lg:w-1/4 ">
                     <img class="rounded-t-lg w-full h-[300px] md:h-[400px] object-cover" src="https://picsum.photos/200/300" alt="">
-                    <div id="content" class="p-2 flex flex-col rounded-b-lg bg-slate-400">
+                    <div id="content" class="p-2 flex flex-col rounded-b-lg bg-[#507e76] text-white">
                         <h3 class="text-lg shadow-lg border-b mb-3 self-center">Book Title</h3>
                         <p>Author: Jon</p>
                         <p>Category: <span class="category">IT</span></p>
