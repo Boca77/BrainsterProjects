80675c6666c0fc94a2fc502c6413cf8a727ecd53
Boris Nacev <bnachev77@gmail.com>
2024-06-08 14:37:03 +0200

    code fixes to singup/login form

--- js/signup-login-btn.js
+++ js/signup-login-btn.js
@@ -7,53 +7,53 @@ if (
   localStorage.getItem("onPage") == "signUp" ||
   !localStorage.getItem("onPage")
 ) {
-  signupForm.classList.add("block");
-  signupForm.classList.remove("hidden");
-
-  loginForm.classList.remove("block");
-  loginForm.classList.add("hidden");
+  handleSignUp();
 }
 
-if (localStorage.getItem("onPage") == "signUp") {
-  signupForm.classList.add("block");
-  signupForm.classList.remove("hidden");
-
-  loginForm.classList.remove("block");
-  loginForm.classList.add("hidden");
+if (localStorage.getItem("onPage") == "logIn") {
+  handleLogIn();
 }
 
 loginBtn.addEventListener("click", () => {
-  loginBtn.style.backgroundColor = "#68b1a5";
+  let onPage = "logIn";
+
+  localStorage.setItem("onPage", onPage);
+
+  handleLogIn();
   loginBtn.style.transition = "all 0.8s ease";
+  signupBtn.style.transition = "all 0.8s ease";
+});
+
+signupBtn.addEventListener("click", () => {
+  let onPage = "signUp";
+
+  localStorage.setItem("onPage", onPage);
+
+  handleSignUp();
+  signupBtn.style.transition = "all 0.8s ease";
+  loginBtn.style.transition = "all 0.8s ease";
+});
+
+function handleSignUp() {
+  signupBtn.style.backgroundColor = "#68b1a5";
+
+  loginBtn.style.backgroundColor = "#487a72";
+
+  signupForm.classList.add("block");
+  signupForm.classList.remove("hidden");
+
+  loginForm.classList.remove("block");
+  loginForm.classList.add("hidden");
+}
+
+function handleLogIn() {
+  loginBtn.style.backgroundColor = "#68b1a5";
 
   signupBtn.style.backgroundColor = "#487a72";
-  signupBtn.style.transition = "all 0.8s ease";
 
   loginForm.classList.add("block");
   loginForm.classList.remove("hidden");
 
   signupForm.classList.remove("block");
   signupForm.classList.add("hidden");
-
-  let onPage = "login";
-
-  localStorage.setItem("onPage", onPage);
-});
-
-signupBtn.addEventListener("click", () => {
-  signupBtn.style.backgroundColor = "#68b1a5";
-  signupBtn.style.transition = "all 0.8s ease";
-
-  loginBtn.style.backgroundColor = "#487a72";
-  loginBtn.style.transition = "all 0.8s ease";
-
-  signupForm.classList.add("block");
-  signupForm.classList.remove("hidden");
-
-  loginForm.classList.remove("block");
-  loginForm.classList.add("hidden");
-
-  let onPage = "signUp";
-
-  localStorage.setItem("onPage", onPage);
-});
+}
