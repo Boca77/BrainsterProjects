ab3426e47857810a1f05b8e5e0cf8df2de0d99de
Boris Nacev <bnachev77@gmail.com>
2024-06-09 10:53:14 +0200

    merged login logic

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+session_start();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -13,9 +17,17 @@
     <div class="banner min-h-[60vh] flex flex-col gap-48 bg-[url('./imgs/banner1.jpg')] bg-cover bg-center border-b-2 border-gray-400 ">
         <nav class="flex justify-between px-10 py-5 justify-self-start">
             <p class="text-xl text-neutral-300">Brainster Library</p>
-            <div class="buttons flex gap-7">
-                <a href="./login-signup.php"><button class="text-white bg-[#1B3534] hover:bg-[#2c5755] focus:outline-none font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2">SignUp</button></a>
-                <a href="./login-signup.php"><button class="text-white bg-[#1B3534] hover:bg-[#2c5755] focus:outline-none font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2">LogIn</button></a>
+            <div class="buttons flex items-center gap-7">
+                <?php
+                $isLoggedIn = $_SESSION['isLoggedIn'] ?? '';
+                if ($isLoggedIn == true) {
+                    echo "<h5 class='text-white'>Welcome {$_SESSION['user']} </h5>";
+                    echo '<a href="./backEnd/logout.php"><button class="text-white bg-[#1B3534] hover:bg-[#2c5755] focus:outline-none font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2">LogOut</button></a>';
+                } else {
+                    echo '<a href="./login-signup.php"><button class="text-white bg-[#1B3534] hover:bg-[#2c5755] focus:outline-none font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2">SignUp / LogIn</button></a>
+                ';
+                }
+                ?>
             </div>
         </nav>
         <h1 class="text-6xl justify-self-center text-white bg-[#1B3534] rounded w-1/2 self-end p-6">Welcome to Brainster library</h1>
