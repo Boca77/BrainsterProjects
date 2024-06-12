f0254c20350b597208dff6a1426e717b390d1a8c
Boris Nacev <bnachev77@gmail.com>
2024-06-12 19:44:08 +0200

    fix bug

--- backEnd/admin/addAuthor.php
+++ backEnd/admin/addAuthor.php
@@ -6,7 +6,7 @@ use Connection\Connection;
 $db = new Connection();
 $connection = $db->getConnection();
 
-$getAuthor = $connection->prepare("SELECT `first_name`, `last_name` FROM `authors` WHERE `first_name` = :name AND `last_name` = :last_name");
+$getAuthor = $connection->prepare("SELECT `first_name`, `last_name` FROM `authors` WHERE `first_name` = :first_name AND `last_name` = :last_name");
 $getAuthor->bindParam(':first_name', $_POST['first_name']);
 $getAuthor->bindParam(':last_name', $_POST['last_name']);
 $getAuthor->execute();
